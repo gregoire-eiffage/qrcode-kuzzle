9ee0a6707a2d099e4c659db616940201c42737f5
gregoire.eiffage <gregoire.lamoureux@eiffage.com>
2025-04-15 14:20:13 +0200

    feat: set up initial project structure with Nuxt 3, including main CSS and QR code module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,41 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
 
   modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/test-utils',
-    '@nuxt/ui'
-  ]
-})
+    "nuxt-qrcode",
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/scripts",
+    "@nuxt/test-utils",
+    "@nuxt/ui",
+  ],
+  css: ["~/assets/css/main.css"],
+  qrcode: {
+    options: {
+      variant: "pixelated",
+      // OR
+      // variant: {
+      //   inner: "circle",
+      //   marker: "rounded",
+      //   pixel: "rounded",
+      // },
+      radius: 1,
+      blackColor: "var(--ui-text-highlighted)", // 'var(--ui-text-highlighted)' if you are using `@nuxt/ui` v3
+      whiteColor: "var(--ui-bg)",
+    },
+  },
+  runtimeConfig: {
+    secret: process.env.SECRET,
+    public: {
+      kuzzleHost: process.env.KUZZLE_HOST,
+      kuzzlePort: process.env.KUZZLE_PORT,
+      kuzzleLocal: process.env.KUZZLE_LOCAL,
+      kuzzleSSL: process.env.KUZZLE_SSL,
+    },
+  },
+});
