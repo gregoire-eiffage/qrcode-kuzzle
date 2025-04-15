cdd2ab7508163637a41e4349219974d4a8f81a2b
gregoire.eiffage <gregoire.lamoureux@eiffage.com>
2025-04-15 14:20:40 +0200

    feat: implement QR Code scanner and result display pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,41 @@
 <template>
-  <div>index</div>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <h1 class="text-3xl font-bold mb-4">QR Code Scanner</h1>
+    <div class="w-48 h-48 rounded-lg shadow-lg overflow-hidden mx-6">
+      <QrcodeStream
+        class="w-full aspect-square rounded-xl"
+        @error="onError"
+        @detect="onDetect"
+      />
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+const toast = useToast();
+onMounted(() => {
+  // Check if the device supports camera
+  if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+    toast.add({
+      title: "Appareil photo non pris en charge",
+      duration: 3000,
+    });
+  }
+});
+
+function onDetect(detectedCodes: DetectedBarcode[]) {
+  if (detectedCodes.length > 0) {
+    const qrCodeValue = detectedCodes[0].rawValue;
+    navigateTo(`/qrcode-value?qrcode=${encodeURIComponent(qrCodeValue)}`);
+  }
+}
+
+function onError(err: Error) {
+  console.error(err);
+  toast.add({
+    title: "Erreur de lecture du QR Code",
+    duration: 3000,
+    icon: "memory:alert",
+  });
+}
+</script>
---
+++ pages/qrcode-value/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex flex-col items-center justify-center h-screen m-4 relative">
+    <h1 class="text-3xl font-bold mb-4">QR Code Scanner</h1>
+    <div class="w-full max-w-md p-4 bg-white rounded-lg">
+      <p class="text-lg font-semibold text-black mb-2">{{ qrCodeValue }}</p>
+    </div>
+    <!-- back to qr code scanner -->
+    <div class="absolute bottom-6 w-full text-center">
+      <nuxt-link to="/" class="text-gray-500 hover:underline">
+        Back to QR Code Scanner
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+import { ref, onMounted } from "vue";
+
+const route = useRoute();
+const qrCodeValue = ref("");
+
+onMounted(() => {
+  qrCodeValue.value = route.query.qrcode as string;
+});
+</script>
